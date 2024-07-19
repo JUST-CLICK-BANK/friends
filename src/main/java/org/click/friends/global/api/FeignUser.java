e4aedc244d61bf40b82597901e74a06312164c56
ParkMiram <myurm@naver.com>
2024-07-19 09:50:59 +0900

    [Fix] auth-feign 수정

--- src/main/java/org/click/friends/global/api/FeignUser.java
+++ src/main/java/org/click/friends/global/api/FeignUser.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "feignUser", url = "192.168.0.16:8080")
+@FeignClient(name = "feignUser", url = "34.30.12.64:31000")
 public interface FeignUser {
 
     @GetMapping(value = "/api/v1/auth/friends")
--- src/main/java/org/click/friends/global/dto/UserListResponse.java
+++ src/main/java/org/click/friends/global/dto/UserListResponse.java
@@ -4,13 +4,15 @@ import java.util.UUID;
 
 public record UserListResponse(
     UUID id,
+    String code,
     String img,
     String name
 ) {
 
-    public static UserListResponse from(UUID id, String img, String name) {
+    public static UserListResponse from(UUID id, String code, String img, String name) {
         return new UserListResponse(
             id,
+            code,
             img,
             name
         );
