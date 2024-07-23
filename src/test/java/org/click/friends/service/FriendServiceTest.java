88b9e2a2252bbdcbbb5ce8b3799bcbc5dd6c8022
ParkMiram <myurm@naver.com>
2024-07-23 17:36:53 +0900

    [Test] service Test 코드 작성

--- src/test/java/org/click/friends/service/FriendServiceTest.java
+++ src/test/java/org/click/friends/service/FriendServiceTest.java
@@ -1,25 +1,26 @@
 package org.click.friends.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+
 import jakarta.transaction.Transactional;
-import org.click.friends.dto.request.FriendRequest;
-import org.click.friends.entity.Friend;
+import org.click.friends.TestInitData;
+import org.click.friends.dto.request.ConfirmFriendRequest;
 import org.click.friends.global.api.ApiUser;
-import org.click.friends.global.dto.UserListResponse;
 import org.click.friends.repository.FriendRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
-import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
-class FriendServiceTest {
+class FriendServiceTest extends TestInitData {
     @InjectMocks
     private FriendServiceImpl friendService;
     @Mock
@@ -27,30 +28,37 @@ class FriendServiceTest {
     @Mock
     ApiUser apiUser;
 
+    String myCode = "AAAAA";
+    String targetCode = "BBBBB";
+
     @Nested
-    @Transactional
     class getFriendList {
         @Test
-        void 성공_친구_목록을_조회함() {
+        void 성공_친구_목록_조회_친구가_있음() {
             // given
-            Friend friend1 = new Friend(1L, true, UUID.fromString("e9b1c7b8-775d-4d7c-b47c-f3c2c43c8658"), UUID.fromString("a0eeb2fc-9994-4e28-b3e3-4c34f88a67f5"));
-            Friend friend2 = new Friend(2L, true, UUID.fromString("a0eeb2fc-9994-4e28-b3e3-4c34f88a67f5"), UUID.fromString("e9b1c7b8-775d-4d7c-b47c-f3c2c43c8658"));
-
-            List<Friend> friendList = List.of(friend1, friend2);
-            List<UserListResponse> userResponseList = List.of(
-                    new UserListResponse(friend1.getMyId(), "AAAAA", "profileImg", "유저1"),
-                    new UserListResponse(friend2.getMyId(), "AAAAA", "profileImg", "유저2")
-            );
-            // Mocking
-            Mockito.when(friendRepository.findByFriendshipIsTrueAndMyId(friend1.getMyId())).thenReturn(friendList);
-//            Mockito.when(apiUser.getUsers()).thenReturn(userResponseList);
+            BDDMockito.given(friendRepository.findByFriendshipIsTrueAndMyCode(myCode))
+                .willReturn(List.of(friend1));
 
             // when
-            List<UserListResponse> friends = friendService.getFriends(friend1.getMyId());
+            friendService.getFriends(myCode);
+
             // then
-            Assertions.assertEquals(2, friends.size());
-            Assertions.assertEquals(UUID.fromString("e9b1c7b8-775d-4d7c-b47c-f3c2c43c8658"), friends.get(0).id());
-            Assertions.assertEquals(UUID.fromString("a0eeb2fc-9994-4e28-b3e3-4c34f88a67f5"), friends.get(1).id());
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .findByFriendshipIsTrueAndMyCode(myCode);
+        }
+
+        @Test
+        void 성공_친구_목록_조회_친구가_없음() {
+            // given
+            BDDMockito.given(friendRepository.findByFriendshipIsTrueAndMyCode(myCode))
+                .willReturn(List.of());
+
+            // when
+            friendService.getFriends(myCode);
+
+            // then
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .findByFriendshipIsTrueAndMyCode(myCode);
         }
     }
 
@@ -60,65 +68,225 @@ class FriendServiceTest {
         @Test
         void 성공_친구_요청을_보냄() {
             // given
-            UUID myId = UUID.fromString("e9b1c7b8-775d-4d7c-b47c-f3c2c43c8658");
-            UUID targetId = UUID.fromString("a0eeb2fc-9994-4e28-b3e3-4c34f88a67f5");
-
-            Friend friend = new Friend(1L, false, myId, targetId);
-            FriendRequest friendRequest = new FriendRequest(friend.getFriendId(), friend.getFriendship(), friend.getTargetId());
-            // TODO Mocking? 아무튼 여기서부터
-            String code = "AAAAA";
+            BDDMockito.given(friendRepository.existsByFriendshipIsFalseAndMyCodeAndTargetCode(myCode, targetCode))
+                .willReturn(false);
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(user);
 
             // when
-            friendService.acceptFriendRequest(code, myId);
+            friendService.acceptFriendRequest(targetCode, myCode);
 
             // then
-            Mockito.verify(friendRepository).save(Mockito.any(Friend.class));
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .save(any());
+        }
+
+        @Test
+        void 실패_없는_유저() {
+            // given
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(null);
+
+            // when
+            assertThrows(IllegalArgumentException.class, () -> friendService.acceptFriendRequest(targetCode, myCode));
+
+            // then
+            Mockito.verify(friendRepository, Mockito.never())
+                .save(Mockito.any());
         }
 
         @Test
         void 실패_이미_요청한_유저() {
+            // given
+            BDDMockito.given(friendRepository.existsByFriendshipIsFalseAndMyCodeAndTargetCode(myCode, targetCode))
+                .willReturn(true);
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(user);
 
+            // when
+            assertThrows(IllegalArgumentException.class, () -> {
+                friendService.acceptFriendRequest(targetCode, myCode);
+            });
+
+            // then
+            Mockito.verify(friendRepository, Mockito.never())
+                .save(Mockito.any());
         }
 
         @Test
         void 실패_이미_친구인_유저() {
+            // given
+            BDDMockito.given(friendRepository.existsByFriendshipIsTrueAndMyCodeAndTargetCode(myCode, targetCode))
+                .willReturn(true);
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(user);
 
+            // when
+            assertThrows(IllegalArgumentException.class, () -> {
+                friendService.acceptFriendRequest(targetCode, myCode);
+            });
+
+            // then
+            Mockito.verify(friendRepository, Mockito.never())
+                .save(Mockito.any());
+        }
+
+        @Test
+        void 실패_나에게_요청했을_경우() {
+            String targetCode = "AAAAA";
+            // given
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(user);
+
+            // when
+            assertThrows(IllegalArgumentException.class, () -> {
+                friendService.acceptFriendRequest(targetCode, myCode);
+            });
+
+            // then
+            Mockito.verify(friendRepository, Mockito.never())
+                .save(Mockito.any());
         }
     }
 
     @Nested
     @Transactional
     class acceptFriendRequest {
+        @Test
         void 성공_받은_요청을_수락함() {
+            // given
+            BDDMockito.given(friendRepository.findByFriendshipIsFalseAndMyCodeAndTargetCode(targetCode, myCode))
+                .willReturn(friend2);
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(user);
+            ConfirmFriendRequest request = new ConfirmFriendRequest(
+                3L,
+                true,
+                targetCode
+            );
 
+            // when
+            friendService.confirmFriendRequest(targetCode, myCode);
+
+            // then
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .save(friend2);
+            Mockito.verify(friendRepository, Mockito.times(2))
+                .save(any());
+        }
+
+        @Test
+        void 실패_없는_유저() {
+            // given
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(null);
+
+            // when
+            assertThrows(IllegalArgumentException.class, () -> friendService.confirmFriendRequest(targetCode, myCode));
+
+            // then
+            Mockito.verify(friendRepository, Mockito.never())
+                .save(Mockito.any());
         }
     }
 
     @Nested
     @Transactional
     class rejectFriendRequest {
+         @Test
         void 성공_받은_요청을_거절함() {
+             // given
+             BDDMockito.given(friendRepository.findByFriendshipIsFalseAndMyCodeAndTargetCode(targetCode, myCode))
+                 .willReturn(friend2);
+             BDDMockito.given(apiUser.getUser(targetCode))
+                 .willReturn(user);
 
+             // when
+             friendService.rejectFriendRequest(targetCode, myCode);
+
+             // then
+             Mockito.verify(friendRepository, Mockito.times(1))
+                 .delete(any());
+        }
+
+        @Test
+        void 실패_없는_유저() {
+            // given
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(null);
+
+            // when
+            assertThrows(IllegalArgumentException.class, () -> friendService.rejectFriendRequest(targetCode, myCode));
+
+            // then
+            Mockito.verify(friendRepository, Mockito.never())
+                .save(Mockito.any());
         }
     }
 
     @Nested
     @Transactional
     class getReceivedFriendRequests {
-        void 성공_받은_요청_목록을_조회함() {
+        @Test
+        void 성공_받은_요청_목록_조회_친구가_있음() {
+            // given
+            BDDMockito.given(friendRepository.findByFriendshipIsFalseAndTargetCode(myCode))
+                .willReturn(List.of(friend2));
 
+            // when
+            friendService.getFriendRequests(myCode);
+
+            // then
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .findByFriendshipIsFalseAndTargetCode(myCode);
+        }
+
+        @Test
+        void 성공_받은_요청_목록_조회_친구가_없음() {
+            // given
+            BDDMockito.given(friendRepository.findByFriendshipIsFalseAndTargetCode(myCode))
+                .willReturn(List.of());
+
+            // when
+            friendService.getFriendRequests(myCode);
+
+            // then
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .findByFriendshipIsFalseAndTargetCode(myCode);
         }
     }
 
     @Nested
     @Transactional
     class deleteFriend {
+        @Test
         void 성공_친구를_삭제함() {
+            // given
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(user);
 
+            // when
+            friendService.removeFriend(targetCode, myCode);
+
+            // then
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .deleteByMyCodeAndTargetCode(myCode, targetCode);
+            Mockito.verify(friendRepository, Mockito.times(1))
+                .deleteByMyCodeAndTargetCode(targetCode, myCode);
         }
 
-        void 실패_이미_친구가_아닌_유저() {
+        @Test
+        void 실패_없는_유저() {
+            // given
+            BDDMockito.given(apiUser.getUser(targetCode))
+                .willReturn(null);
 
+            // when
+            assertThrows(IllegalArgumentException.class, () -> friendService.removeFriend(targetCode, myCode));
+
+            // then
+            Mockito.verify(friendRepository, Mockito.never())
+                .save(Mockito.any());
         }
     }
 }
