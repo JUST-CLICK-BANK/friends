c47e9374b57886f8206ea1af97289db39ebfd78a
ParkMiram <myurm@naver.com>
2024-07-23 19:42:45 +0900

    [Refactor] Friend -> Friends

--- src/test/java/org/click/friends/TestInitData.java
+++ src/test/java/org/click/friends/TestInitData.java
@@ -1,17 +1,17 @@
 package org.click.friends;
 
 import java.util.UUID;
-import org.click.friends.entity.Friend;
+import org.click.friends.entity.Friends;
 import org.click.friends.global.dto.UserResponse;
 
 public class TestInitData {
-    protected final Friend friend1;
-    protected final Friend friend2;
+    protected final Friends friend1;
+    protected final Friends friend2;
     protected final UserResponse user;
 
     public TestInitData() {
-        this.friend1 = new Friend(1L, true, "AAAAA", "BBBBB");
-        this.friend2 = new Friend(2L, false, "AAAAA", "BBBBB");
+        this.friend1 = new Friends(1L, true, "AAAAA", "BBBBB");
+        this.friend2 = new Friends(2L, false, "AAAAA", "BBBBB");
         this.user = new UserResponse(
             UUID.fromString("f47ac10b-58cc-4372-a567-0e02b2c3d479"),
             "BBBBB",
